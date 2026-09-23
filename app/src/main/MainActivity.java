63431b40588968b7fd6684e161f0f7f908e6deba
masoudkhosropour-sketch <masoud.khosropour@gmail.com>
2026-09-23 15:37:43 +0330

    Update and rename mainactivity.java to MainActivity.java

--- app/src/main/MainActivity.java
+++ app/src/main/MainActivity.java
@@ -2,6 +2,7 @@ package com.carlauncher;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.graphics.Color;
 import android.view.Gravity;
 import android.widget.TextView;
 
@@ -12,9 +13,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         TextView text = new TextView(this);
+
         text.setText("CAR LAUNCHER");
         text.setTextSize(32);
+        text.setTextColor(Color.WHITE);
         text.setGravity(Gravity.CENTER);
+        text.setBackgroundColor(Color.BLACK);
 
         setContentView(text);
     }
